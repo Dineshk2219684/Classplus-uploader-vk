475c0f31e0151be4ee592acedf49a751865e139d
Dineshk2219684 <100016846+Dineshk2219684@users.noreply.github.com>
2025-12-24 19:25:41 +0530

    Update API_ID and API_HASH in vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,8 +3,8 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22694914"))
-API_HASH = environ.get("API_HASH", "4fd02c02368941a03d48ecab3a3b4a31")
+API_ID = int(environ.get("API_ID", "26851879"))
+API_HASH = environ.get("API_HASH", "aac0cd7d51dfc91c5bc70c26763134bf")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 OWNER = int(environ.get("OWNER", "7491374623"))
@@ -18,4 +18,5 @@ AUTH_USER = os.environ.get('AUTH_USERS', '7491374623').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
+
 
